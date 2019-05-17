9c9e15a7c2fbd06879d1c768ea7475c6143d3fb0
Shantaram K <shantaramk@mipladmin-6.local>
2019-05-17 12:45:30 +0530

    Initial Commit

---
+++ ImagePreview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImagePreview
+//
+//  Created by Shantaram K on 17/05/19.
+//  Copyright © 2019 Shantaram Kokate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
