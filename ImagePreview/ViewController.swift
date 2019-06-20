9d39a1a167fc0133e0f0270d70778a54ec35d185
Shantaram K <shantaramk@Mipladmin-6.local>
2019-06-20 12:07:11 +0530

    New changes

--- ImagePreview/ViewController.swift
+++ ImagePreview/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func showProfile() {
+        let vc: ImagePreviewController = UIStoryboard.controller(storyboard: .main)
+       // vc.imageURLString = message.profilePicLink
+        vc.image = #imageLiteral(resourceName: "car")
+         self.present(vc, animated: true, completion: nil)
+    }
 
 }
 
